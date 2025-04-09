8762812c4fc76328505fc4e62781b4db47ecafef
Dalso13 <flskwhdhk@gmail.com>
2025-04-09 14:43:53 +0900

    feat : 검색 엔진 구현

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AutoComplete from "./components/AutoComplete.vue";
+export default {
+  components: {
+    AutoComplete
+  },
+  data() {
+    return {
+      searchData : "",
+      classTag : "search-form",
+      autoSearchDatas: [
+          "apple",
+          "awesome",
+          "animal",
+          "banana",
+          "born",
+          "butterfly",
+          "cat",
+          "creative",
+          "cookie",
+      ],
+      currentDatas: [],
+    }
+  },
+  methods: {
+    changeText(e) {
+      this.searchData = e.target.value    
+      if (this.searchData.length <= 0) {
+        this.classTag = "search-form"
+      } else if (this.classTag == "search-form") {
+        this.classTag = "search-form active";
+      }
+      this.currentDatas = this.autoSearchDatas.filter((v) => v.includes(this.searchData));
+    },
+    classActive() {
+      if (this.searchData.length > 0) this.classTag = "search-form active";
+    },
+  },
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div v-bind:class="classTag" role="search">
+        <div class="input-group">
+          <button type="button" aria-label="Search">
+            <i class="fas fa-search"></i>
+          </button>
+          <input
+            :value="searchData"
+            @input="changeText"
+            @focus="classActive"
+            type="text"
+            placeholder="Search"
+            aria-autocomplete="list"
+            aria-controls="auto-complete"
+          />
+          <button type="button" aria-label="Microphone">
+            <i class="fas fa-microphone"></i>
+          </button>
+        </div>
+        <ul
+          class="auto-complete"
+          id="auto-complete"
+          role="listbox"
+          aria-expanded="false"
+          aria-hidden="true"
+        >
+        </ul>
+    </div>
 </template>
-
+  
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
